975df851338255e3cb7149f1b248a540eb0b4632
Robert Colin O'Beirne <robeirne@uncc.edu>
2023-02-23 18:43:38 -0500

    Add project files.

---
+++ CKC App 4155/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CKC_App_4155;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ CKC App 4155/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CKC_App_4155;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
