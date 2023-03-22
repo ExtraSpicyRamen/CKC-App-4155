cc7665a670f9fcf1ba608ea93428f7859190ca6f
reedholz <rholz@uncc.edu>
2023-03-21 22:37:57 -0400

    Basic User Settings Page (will change)

--- CKC App 4155/CreateAccount.xaml.cs
+++ CKC App 4155/CreateAccount.xaml.cs
@@ -4,6 +4,6 @@ public partial class CreateAccount : ContentPage
 {
 	public CreateAccount()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
 }
---
+++ CKC App 4155/UserSettings.xaml.cs
@@ -0,0 +1,9 @@
+namespace CKC_App_4155;
+
+public partial class UserSettings : ContentPage
+{
+	public UserSettings()
+	{
+        InitializeComponent();
+	}
+}
