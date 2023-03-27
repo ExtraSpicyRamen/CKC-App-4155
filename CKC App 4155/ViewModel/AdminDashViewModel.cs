d1bbc95a7b5c6d0d74586114ad3fb0c6193c2b04
MiffleS2 <chanson7@uncc.edu>
2023-03-27 16:49:23 -0400

    Added AdminDashViewModel and AdminDash functionality with navigation

---
+++ CKC App 4155/ViewModel/AdminDashViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CKC_App_4155.ViewModel
+{
+    public partial class AdminDashViewModel : ObservableObject
+    {
+        [RelayCommand]
+        async Task Navigate() =>
+            await AppShell.Current.GoToAsync(nameof(EventDetailPage));
+    }
+}
