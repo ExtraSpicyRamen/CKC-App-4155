55e955de111e67df8ac7e692a4dd0f3fe546086e
Bryan Xiong <xiongbryan5@gmail.com>
2023-04-24 17:54:54 -0400

    Created majority of frontend on message creation.

--- CKC App 4155/CreateMessagePage.xaml.cs
+++ CKC App 4155/CreateMessagePage.xaml.cs
@@ -1,9 +1,33 @@
+using CKC_App_4155.objects;
+
 namespace CKC_App_4155;
 
 public partial class CreateMessagePage : ContentPage
 {
-	public CreateMessagePage()
+	Message newMsg;
+	List<string> users;
+
+    public CreateMessagePage()
 	{
+		newMsg = new Message();
 		InitializeComponent();
+		Random random = new Random();
+        BasicSimulation();
+		int generatedID = random.Next(0, 100);
+		newMsg.SetMessageID(generatedID);
 	}
+
+    private void TitleTextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+	public void BasicSimulation()
+	{
+        users = new List<string>();
+		users.Add("Bobby");
+		users.Add("James");
+		users.Add("Tiffany");
+		users.Add("Beth");
+		receiverPicker.ItemsSource = users;
+    }
 }
