519823976be47bdbb1a4e928fc9a760b955edabf
Bryan Xiong <bxiong11@uncc.edu>
2023-04-24 14:40:35 -0400

    establish view for looking at recieved and sent message

--- CKC App 4155/ViewMessagesPage.xaml.cs
+++ CKC App 4155/ViewMessagesPage.xaml.cs
@@ -1,9 +1,20 @@
+using CKC_App_4155.objects;
+
 namespace CKC_App_4155;
 
 public partial class ViewMessagesPage : ContentPage
 {
+	Message message;
 	public ViewMessagesPage()
 	{
-		InitializeComponent();
-	}
+		Message message1 = new Message(1, 1, 2, "Testing sent message", "this iss the passage that I wrote for the test.", "John", "Betty");
+        Message message2 = new Message(2, 1, 1, "Testing recieved message", "this iss the passage that I wrote for the test.", "Betty", "John");
+        InitializeComponent();
+		List<Message> sentMesages = new List<Message>();
+        sentMesages.Add(message1);
+		sentMesages.Add(message2);
+		listofSentMessages.ItemsSource = sentMesages;
+        listofReceivedMessages.ItemsSource = sentMesages;
+
+    }
 }
