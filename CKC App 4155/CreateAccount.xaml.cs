c69df08260cf37bba72b0e70eec1dd49ad899190
reedholz <rholz@uncc.edu>
2023-04-26 07:22:46 -0400

    Added signup logic

--- CKC App 4155/CreateAccount.xaml.cs
+++ CKC App 4155/CreateAccount.xaml.cs
@@ -1,3 +1,7 @@
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using System.Diagnostics;
+
 namespace CKC_App_4155;
 
 public partial class CreateAccount : ContentPage
@@ -6,4 +10,38 @@ public partial class CreateAccount : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void SignUp(object sender, EventArgs e)
+    {
+        var config = new FirebaseAuthConfig
+        {
+            ApiKey = "AIzaSyBH_-jT47UI-yQg6G5bKnZA3C0Cg5lxpPM",
+            AuthDomain = "ckc-app-4155.firebaseapp.com",
+            Providers = new FirebaseAuthProvider[]
+        {
+            new GoogleProvider().AddScopes("email"),
+            new EmailProvider()
+        },
+        };
+
+        var client = new FirebaseAuthClient(config);
+        String email = UserEmail.Text;
+        String userName = UserName.Text;
+        String phoneNumber = UserPhoneNumber.Text;
+        String password = UserPassword.Text;
+        String confirmPassword = UserConfirmPassword.Text;
+
+        if (password != confirmPassword)
+        {
+            // Display an error message if the passwords do not match
+            await DisplayAlert("Error", "Passwords do not match", "OK");
+            return;
+        }
+
+        var userCredential = await client.CreateUserWithEmailAndPasswordAsync(email, password);
+
+        var user = userCredential.User;
+        var uid = user.Uid;
+        Debug.WriteLine(uid + " signed in");
+    }
 }
