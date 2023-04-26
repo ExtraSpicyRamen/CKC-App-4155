ccf2486c5b9b7f9ac63a7c6594c7a4d58f83584e
reedholz <rholz@uncc.edu>
2023-04-26 07:23:13 -0400

    Added sign in logic

--- CKC App 4155/MainPage.xaml.cs
+++ CKC App 4155/MainPage.xaml.cs
@@ -1,13 +1,17 @@
-﻿namespace CKC_App_4155;
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+using System.Diagnostics;
+
+namespace CKC_App_4155;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-	async void GotoCreateAccount(object sender, EventArgs e)
-	{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    async void GotoCreateAccount(object sender, EventArgs e)
+    {
         await Navigation.PushAsync(new CreateAccount());
     }
     async void GotoForgotPassword(object sender, EventArgs e)
@@ -15,9 +19,37 @@ public partial class MainPage : ContentPage
         await Navigation.PushAsync(new ForgotPasswordPage());
     }
 
-    async void GotoHomePage(object sender, EventArgs e)
+    private async void SignIn(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new HomePage());
+        var config = new FirebaseAuthConfig
+        {
+            ApiKey = "AIzaSyBH_-jT47UI-yQg6G5bKnZA3C0Cg5lxpPM",
+            AuthDomain = "ckc-app-4155.firebaseapp.com",
+            Providers = new FirebaseAuthProvider[]
+        {
+            new GoogleProvider().AddScopes("email"),
+            new EmailProvider()
+        },
+        };
+
+        var client = new FirebaseAuthClient(config);
+        String email = UserEmail.Text;
+        String password = UserPassword.Text;
+
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            // Display an error message if the email or password is not entered
+            await DisplayAlert("Error", "Please enter your email and password", "OK");
+            return;
+        }
+
+        var userCredential = await client.SignInWithEmailAndPasswordAsync(email, password);
+
+        var user = userCredential.User;
+        var uid = user.Uid;
+        Debug.WriteLine(uid + " signed in");
     }
-}
+
+    }
+
 
